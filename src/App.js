7f80ce317aebeb4a62bef090a6d2c2c4c631eaaa
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-10 16:10:46 -0300

    exclui arquivos de testes no App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./global.sass";
-import st from "./styles/teste.module.sass";
 import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <div className="teste">
-          <h1 className={st.teste}>Título</h1>
-        </div>
-        <ul className={st.segundoTeste}>
-          <li>um</li>
-          <li>dois</li>
-          <li>tres</li>
-        </ul>
-      </main>
+      <main></main>
     </div>
   );
 }
