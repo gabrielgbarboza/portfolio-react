ebce70f5fecd4a9d0dbbcb30d0e475f7ba0d1376
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-11 10:27:07 -0300

    Adição da função Introducao no App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./global.sass";
 import Header from "./components/Header.jsx";
+import Introducao from "./components/Introducao.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main></main>
+      <main>
+        <Introducao />
+      </main>
     </div>
   );
 }
