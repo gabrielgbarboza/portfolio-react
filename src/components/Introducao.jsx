03c1f1a92836e30c3d69c871932b27e732ef578d
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-26 14:43:09 -0300

    ajusta detalhes na introducão

--- src/components/Introducao.jsx
+++ src/components/Introducao.jsx
@@ -2,6 +2,7 @@ import logoLinkedin from "../assets/img/logoLinkedin.svg";
 import logoGithub from "../assets/img/logoGithub.svg";
 import gabrielIntroducao from "../assets/img/gabrielIntroducao.png";
 import st from "../assets/styles/Introducao.module.sass";
+import cn from "classnames";
 
 export default function Introducao() {
   return (
