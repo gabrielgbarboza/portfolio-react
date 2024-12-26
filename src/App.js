70c9809fdcc9b6b2eb7fb37de7fb45f9c1bcd701
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-26 14:41:53 -0300

    adicionando o componente Projetos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./assets/styles/global.sass";
-import Header from "./components/Header.jsx";
-import Introducao from "./components/Introducao.jsx";
+import './assets/styles/global.sass';
+import Header from './components/Header.jsx';
+import Introducao from './components/Introducao.jsx';
+import Projetos from './components/Projetos.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <main>
         <Introducao />
+        <Projetos />
       </main>
     </div>
   );
