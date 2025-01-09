1277ec8d575ab8947ac467461873ea3b46b58da3
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2025-01-09 10:41:02 -0300

    Adicionando o Cursos no App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/styles/global.sass';
 import Header from './components/Header.jsx';
 import Introducao from './components/Introducao.jsx';
 import Projetos from './components/Projetos.jsx';
+import Cursos from './components/Cursos.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Introducao />
         <Projetos />
+        <Cursos />
       </main>
     </div>
   );
