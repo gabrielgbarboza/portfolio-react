60c2e011d560afe369290235998543dc4d5c9119
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2025-01-23 14:56:29 -0300

    alterando value e name da tag form

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -38,14 +38,14 @@ export default function Contato() {
         <div className={st.contatoFormulario}>
           <form
             id='formulario'
-            name='formulario-contato'
+            name='forms-contato'
             method='POST'
             data-netlify='true'
             netlify-honeypot='bot-field'
             action='/'
           >
             {/* Formulario Netlify */}
-            <input type='hidden' name='form-name' value='formulario-contato' />
+            <input type='hidden' name='form-name' value='forms-contato' />
             <input type='hidden' name='bot-field' />
             <label className='text' htmlFor='Nome'>
               Nome
